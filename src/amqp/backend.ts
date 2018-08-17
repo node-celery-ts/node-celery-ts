6e69403fa3d3039d10a521c2ab2327002da75520
Gregory Meyer <Gregory.James.Meyer@ibm.com>
2018-08-16 22:10:37 -0400

    [docs] update documentation for Amqp.Backend
    References #7

--- src/amqp/backend.ts
+++ src/amqp/backend.ts
@@ -58,8 +58,9 @@ export class RpcBackend implements ResultBackend {
      * Constructs an RpcBackend with the given routing key and options.
      * Queues up connection, creation, queue assertion, and consumer handling.
      *
-     * @param routingKey the routing key / queue name to consume from
-     * @param Options the options for the amqp connection
+     * @param routingKey The name of the queue to consume from.
+     * @param options Connection options for the RabbitMQ connections. If
+     *                `undefined`, will connect to localhost:6379.
      */
     public constructor(routingKey: string, options?: AmqpOptions) {
         const DEFAULT_TIMEOUT: number = 86400000; // 1 year in milliseconds
@@ -94,8 +95,8 @@ export class RpcBackend implements ResultBackend {
     }
 
     /**
-     * @param message the message to put on the backend
-     * @returns a promise that resolves to the response from the backend
+     * @param message The message to queue.
+     * @returns The response from RabbitMQ.
      */
     public put<T>(message: ResultMessage<T>): Promise<string> {
         const toSend = Buffer.from(JSON.stringify(message), "utf8");
@@ -113,9 +114,12 @@ export class RpcBackend implements ResultBackend {
     }
 
     /**
-     * @param taskId The task ID whose result we want to fetch.
-     * @param timeout The time to wait before rejecting the promise.
-     * @returns A  promise with the response from the backend.
+     * Uses `Utility.createTimeoutPromise`.
+     *
+     * @param taskId The UUID of the task whose result is to be fetched.
+     * @param timeout The time to wait, im milliseconds, before rejecting
+     *                the promise. If `undefined`, will wait forever.
+     * @returns The result as fetched from RabbitMQ.
      */
     public get<T>({ taskId, timeout }: GetOptions): Promise<ResultMessage<T>> {
         const result = this.promises.get(taskId)
@@ -125,8 +129,8 @@ export class RpcBackend implements ResultBackend {
     }
 
     /**
-     * @param taskId the task ID whose result we want to delete
-     * @returns a promise with the response from the backend
+     * @param taskId The UUID of the task whose result is to be deleted.
+     * @returns "deleted" | "no result found".
      */
     public delete(taskId: string): Promise<string> {
         const response = (() => {
@@ -141,7 +145,9 @@ export class RpcBackend implements ResultBackend {
     }
 
     /**
-     * gently terminates the connection with the backend
+     * Gently closes all channels and the connection with RabbitMQ.
+     *
+     * @returns A `Promise` that resolves when the disconnection is complete.
      */
     public disconnect(): Promise<void> {
         return Promise.all([
@@ -161,12 +167,27 @@ export class RpcBackend implements ResultBackend {
     }
 
     /**
-     * @returns the backend URI
+     * TODO: Actually return the connected RabbitMQ node's URI.
+     *
+     * @returns Nothing.
+     *
+     * @throws UnimplementedError Always.
      */
-    public uri(): string {
+    public uri(): never {
         throw new UnimplementedError("Celery.Amqp.Backend.RpcBackend.uri");
     }
 
+    /**
+     * Converts a message, assumed to be UTF-8 encoded, into an object
+     * representation.
+     *
+     * @param message The raw message to parse.
+     * @returns An object representation of `message`'s contents.
+     *
+     * @throws Error If the message does not contain valid UTF-8.
+     * @throws SyntaxError If the message does not contain a valid
+     *                     JSON-serialized object.
+     */
     private static parseMessage<T>(message: Message): ResultMessage<T> {
         const content = message.content.toString("utf8");
         const parsed: ResultMessage<T> = JSON.parse(content);
@@ -174,8 +195,17 @@ export class RpcBackend implements ResultBackend {
         return parsed;
     }
 
-    private static createPublishOptions<T>(message: ResultMessage<T>)
-    : AmqpLib.Options.Publish {
+    /**
+     * Creates options for UTF-8 encoding, JSON serialization, non-persistent
+     * transport, and 0 priority, with the UUID (`correlationId`) taken from
+     * `message`.
+     *
+     * @param message The message to create publish options for.
+     * @returns Options for publishing `message`.
+     */
+    private static createPublishOptions<T>(
+        message: ResultMessage<T>
+    ): AmqpLib.Options.Publish {
         return {
             contentEncoding: "utf-8",
             contentType: "application/json",
@@ -185,8 +215,15 @@ export class RpcBackend implements ResultBackend {
         };
     }
 
-    private assertQueue(channel: AmqpLib.Channel)
-    : Promise<AmqpLib.Replies.AssertQueue> {
+    /**
+     * Calls `AmqpLib.Channel#assertQueue`.
+     *
+     * @param channel The channel to use.
+     * @returns The reply from RabbitMQ.
+     */
+    private assertQueue(
+        channel: AmqpLib.Channel
+    ): Promise<AmqpLib.Replies.AssertQueue> {
         return Promise.resolve(channel.assertQueue(this.routingKey, {
             autoDelete: false,
             durable: false,
@@ -194,6 +231,14 @@ export class RpcBackend implements ResultBackend {
         }));
     }
 
+    /**
+     * Calls `AmqpLib.Channel#sendToQueue`. If the write buffer is full,
+     * runs in a recursive loop triggered by the `"drain"` event being emitted.
+     *
+     * @param channel The channel to use.
+     * @param options The options to publish with.
+     * @param toSend The payload to write.
+     */
     private sendToQueue({ channel, options, toSend }: {
         channel: AmqpLib.Channel;
         options: AmqpLib.Options.Publish;
@@ -210,6 +255,12 @@ export class RpcBackend implements ResultBackend {
         return Promise.resolve("flushed to write buffer");
     }
 
+    /**
+     * Converts an `AmqpLib.Channel` into a consumer.
+     *
+     * @param consumer The `Channel` to use.
+     * @returns A `Promise` that resolves to the consumer tag of the `Channel`.
+     */
     private createConsumer(consumer: AmqpLib.Channel): Promise<string> {
         return Promise.resolve(consumer.consume(
             this.routingKey,
@@ -218,6 +269,13 @@ export class RpcBackend implements ResultBackend {
          )).then((reply) => reply.consumerTag);
     }
 
+    /**
+     * To run whenever a message is received. If the RabbitMQ server cancels
+     * the consumer, all pending promises will be rejected.
+     *
+     * @param maybeMessage A message received from RabbitMQ. Will be null
+     *                     if the consumer is cancelled.
+     */
     private onMessage(maybeMessage?: Message | null): void {
         if (isNullOrUndefined(maybeMessage)) {
             const error = new Error("RabbitMQ cancelled consumer");
@@ -233,6 +291,9 @@ export class RpcBackend implements ResultBackend {
     }
 }
 
+/**
+ * Layout of a RabbitMQ message.
+ */
 interface Message {
     content: Buffer;
     fields: object;
