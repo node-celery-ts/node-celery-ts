574a94995d217fbbe2cf38325bddeaf83c423853
Gregory Meyer <gregjm@umich.edu>
2018-08-20 18:31:25 -0400

    [test] add more RedisBackend#{delete,get} tests
    References #9

--- vagrant/redis.spec.ts
+++ vagrant/redis.spec.ts
@@ -39,6 +39,12 @@ import * as Uuid from "uuid";
 Mocha.describe("Celery.RedisBackend", () => {
     const REDIS_URI: string = "redis://localhost";
 
+    const createRedisClient = () => new Ioredis(REDIS_URI);
+
+    const getKey = (id: string) => `celery-task-meta-${id}`;
+
+    const createRedisBackend = () => new Celery.RedisBackend();
+
     Mocha.it("should receive messages via SET/GET", async () => {
         const id = Uuid.v4();
         const message = {
@@ -49,14 +55,15 @@ Mocha.describe("Celery.RedisBackend", () => {
             traceback: null,
         };
 
-        const redis = new Ioredis(REDIS_URI);
-        await redis.set(`celery-task-meta-${id}`, JSON.stringify(message));
+        const redis = createRedisClient();
+        await redis.set(getKey(id), JSON.stringify(message));
 
-        const backend = Celery.createBackend("", REDIS_URI);
-        const result = await backend.get<string>({ taskId: id, timeout: 15 });
+        const backend = createRedisBackend();
+        const result = await backend.get<string>({ taskId: id, timeout: 20 });
 
         Chai.expect(result).to.deep.equal(message);
 
+        await backend.end();
         await redis.flushall();
         await redis.quit();
         redis.disconnect();
@@ -72,14 +79,17 @@ Mocha.describe("Celery.RedisBackend", () => {
             traceback: null,
         };
 
-        const backend = Celery.createBackend("", REDIS_URI);
-        const result = backend.get<string>({ taskId: id, timeout: 15 });
+        const backend = createRedisBackend();
+        const result = backend.get<string>({ taskId: id, timeout: 20 });
 
-        const redis = new Ioredis(REDIS_URI);
-        await redis.publish(`celery-task-meta-${id}`, JSON.stringify(message));
+        await new Promise((resolve) => setTimeout(resolve, 5));
+
+        const redis = createRedisClient();
+        await redis.publish(getKey(id), JSON.stringify(message));
 
         Chai.expect(await result).to.deep.equal(message);
 
+        await backend.end();
         await redis.flushall();
         await redis.quit();
         redis.disconnect();
@@ -95,17 +105,68 @@ Mocha.describe("Celery.RedisBackend", () => {
             traceback: null,
         };
 
-        const redis = new Ioredis(REDIS_URI);
+        const redis = createRedisClient();
 
-        const backend = Celery.createBackend("", REDIS_URI);
+        const backend = createRedisBackend();
         await backend.put<string>(message);
 
-        const expry = await redis.ttl(`celery-task-meta-${id}`);
-        const placed = JSON.parse(await redis.get(`celery-task-meta-${id}`));
+        const expry = await redis.ttl(getKey(id));
+        const placed = JSON.parse(await redis.get(getKey(id)));
 
         Chai.expect(placed).to.deep.equal(message);
         Chai.expect(expry).to.be.greaterThan(8638); // give 2 seconds leeway
 
+        await backend.end();
+        await redis.flushall();
+        await redis.quit();
+        redis.disconnect();
+    });
+
+    Mocha.it("should DELETE from Redis when #delete is invoked", async () => {
+        const id = Uuid.v4();
+        const message = {
+            children: [],
+            result: "foo",
+            status: Celery.Status.Success,
+            task_id: id,
+            traceback: null,
+        };
+
+        const redis = createRedisClient();
+        await redis.set(getKey(id), JSON.stringify(message));
+
+        const backend = createRedisBackend();
+
+        Chai.expect(await backend.delete(id)).to.deep.equal("1");
+        Chai.expect(await redis.exists(getKey(id))).to.deep.equal(0);
+
+        await backend.end();
+        await redis.flushall();
+        await redis.quit();
+        redis.disconnect();
+    });
+
+    Mocha.it("should receive messages published before #get", async () => {
+        const id = Uuid.v4();
+        const message = {
+            children: [],
+            result: "foo",
+            status: Celery.Status.Success,
+            task_id: id,
+            traceback: null,
+        };
+
+        const backend = createRedisBackend();
+
+        await new Promise((resolve) => setTimeout(resolve, 5));
+
+        const redis = createRedisClient();
+        await redis.publish(getKey(id), JSON.stringify(message));
+
+        Chai.expect(await backend.get<string>({ taskId: id }))
+            .to.deep.equal(message);
+
+        await backend.end();
         await redis.flushall();
         await redis.quit();
         redis.disconnect();
